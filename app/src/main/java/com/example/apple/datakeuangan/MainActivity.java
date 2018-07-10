1618e155931be9dcd4e31249f29ab35baf33ce0e
Achmad Fadhitya <afadhitya@gmail.com>
2018-07-11 05:26:45 +0700

    Add switching and change icon plus

--- app/src/main/java/com/example/apple/datakeuangan/MainActivity.java
+++ app/src/main/java/com/example/apple/datakeuangan/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.apple.datakeuangan;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,18 +17,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    final Context c = this;
 
     TextView totalKotorTV;
     TextView totalHutangTV;
     TextView totalBersihTV;
+    Spinner dariPenyimpananS;
+    Spinner kePenyimpananS;
+    EditText banyaknyaET;
+
+    ArrayList<PenyimpananClass> penyimpananSpinner;
 
     DBControllerPenyimpanan dbPenyimpanan;
     DBControllerHutang dbHutang;
@@ -109,6 +123,52 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    private void loadSpinnerData() {
+        penyimpananSpinner = new ArrayList<PenyimpananClass>();
+
+        // database handler
+        penyimpananSpinner = dbPenyimpanan.getDataPenyimpanan();
+
+        // Spinner Drop down elements
+        List<String> lables = new ArrayList<>();
+
+        for(int i=0;i<penyimpananSpinner.size();i++) {
+            lables.add(penyimpananSpinner.get(i).getNamaPenyimpanan());
+
+        }
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, lables);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        dariPenyimpananS.setAdapter(dataAdapter);
+        kePenyimpananS.setAdapter(dataAdapter);
+    }
+
+    public void switching(int idDari, int idKe, int jumlah){
+        PenyimpananClass penyimpananDari;
+        PenyimpananClass penyimpananKe;
+
+        //Pengurangan dari
+        penyimpananDari = dbPenyimpanan.getPenyimpanan(idDari);
+        int uangDari = penyimpananDari.getIsiPenyimpanan();
+        uangDari = uangDari - jumlah;
+
+        dbPenyimpanan.updateData(idDari, uangDari);
+
+        //Penambahan Ke
+        penyimpananKe = dbPenyimpanan.getPenyimpanan(idKe);
+        int uangKe = penyimpananKe.getIsiPenyimpanan();
+        uangKe = uangKe + jumlah;
+
+        dbPenyimpanan.updateData(idKe, uangKe);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -122,10 +182,48 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+            View mView = layoutInflaterAndroid.inflate(R.layout.dialog_input_switching, null);
+            AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+            alertDialogBuilderUserInput.setView(mView);
+
+            dariPenyimpananS = (Spinner) mView.findViewById(R.id.dariPenyimpanan);
+            kePenyimpananS = (Spinner) mView.findViewById(R.id.kePenyimpanan);
+            banyaknyaET = (EditText) mView.findViewById(R.id.banyaknya);
+
+            loadSpinnerData();
+
+            alertDialogBuilderUserInput
+                    .setCancelable(false)
+                    .setPositiveButton("Switch", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogBox, int id) {
+//                            String keteranganHutang = EditinputHutangKeterangan.getText().toString();
+//                            String banyaknyaHutang = EditinputHutangAmount.getText().toString();
+
+//                            int banyaknyaHutangInt =  Integer.parseInt(banyaknyaHutang);
+
+//                            addHutangKeDB(keteranganHutang, banyaknyaHutangInt);
+
+                            int dariPenyimpanan = penyimpananSpinner.get(dariPenyimpananS.getSelectedItemPosition()).getIdPenyimpanan();
+                            int kePeyimpanan = penyimpananSpinner.get(kePenyimpananS.getSelectedItemPosition()).getIdPenyimpanan();
+                            int banyaknya = Integer.parseInt(banyaknyaET.getText().toString());
+
+                            switching(dariPenyimpanan, kePeyimpanan, banyaknya);
+
+                        }
+                    })
+
+                    .setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialogBox, int id) {
+                                    dialogBox.cancel();
+                                }
+                            });
+
+            AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+            alertDialogAndroid.show();
         }
 
         return super.onOptionsItemSelected(item);
